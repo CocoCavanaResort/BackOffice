6dc9eb2177573f23b6ef0c85872ae915a11f52ff
CocoCavanaResort <cococavanaresort@gmail.com>
2025-01-29 19:21:45 +0800

    Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
     <link rel="stylesheet" href="/public/src/css/login.min.css">
 @endpush
 @section('main')
-    <div class="card wrapper">
+    <div class="card wrapper shadow">
         <div class="card-body">
-            <h4>{{ config('app.name') }}</h4>
+            <h5>{{ config('app.name') }}</h5>
             <div class="graphics">
                 <img src="/public/src/images/login.jpg" alt="">
             </div>
@@ -15,14 +15,14 @@
                     <label for="email" class="form-label">Email</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa fa-envelope"></i></span>
-                        <input class="form-control" type="email" name="email" id="email">
+                        <input class="form-control" type="email" name="email" id="email" autocomplete="off" autofocus required>
                     </div>
                 </div>
                 <div class="mb-4">
                     <label for="password" class="form-label">Password</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="fa fa-lock"></i></span>
-                        <input type="password" class="form-control" name="password" id="password">
+                        <input type="password" class="form-control" name="password" id="password" autocomplete="off" required>
                     </div>
                 </div>
                 <div class="mb-4">
